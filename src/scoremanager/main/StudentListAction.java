e7e3f503e6ff592768a5f0b917f96fe7eab41a4c
oohara <oohara@172.16.31.112>
2024-04-12 15:18:42 +0900

    2回目

--- src/scoremanager/main/StudentListAction.java
+++ src/scoremanager/main/StudentListAction.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -34,5 +35,20 @@ public class StudentListAction extends Action {
 		StudentDao sDao=new StudentDao();//学生Dao
 		ClassNumDao cNumDao=newClassDao();//クラス番号Daoを初期化
 		Map<String, String>errors=new HashMap<>();//エラーメッセージ
+	
+		//リクエストパラメータの取得
+		ServletRequest req;
+		entYearStr = req.getParameter("f1");
+		classNum = req.getParameter("f2");
+		isAttendStr = req.getParameter("f3");
+		
+		List<String>list=cNumDao.filter(teacher.getSchool());
+		
+		if(entYear!=0 && !classNum.equals("0")){
+			students=sDao.filter(teacher.getSchool() entYear, classNum, isAttend);
+		}
+		
 	}
+	
+
 }
