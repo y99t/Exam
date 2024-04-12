9f58fb9d02c1088b6c8008d3e8aacb56910a2993
oohara <oohara@172.16.31.112>
2024-04-12 15:18:27 +0900

    2回目

--- src/dao/StudentDao.java
+++ src/dao/StudentDao.java
@@ -1,6 +1,33 @@
 package dao;
 
+import java.sql.ResultSet;
+import java.util.List;
+
+import bean.School;
+import bean.Student;
+
 public class StudentDao extends Dao {
 
-	private
+	private String baseSql;
+
+	public Student get(String no) throws Exception{
+
+	}
+
+	private List<Student> postFilter(ResultSet rSet, School school) throws Exception{
+
+	}
+
+	public List<Student> filter(School school, int entYear, String classNum, boolean isAttend) throws Exception{
+
+	}
+
+	public List<Student> filter(School school, boolean isAttend) throws Exception{
+
+	}
+
+	public boolean save(Student student) throws Exception {
+
+	}
+
 }
--- src/scoremanager/main/StudentListAction.java
+++ src/scoremanager/main/StudentListAction.java
@@ -1,17 +1,38 @@
 package scoremanager.main;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bean.Student;
+import bean.Teacher;
+import dao.StudentDao;
 import tool.Action;
 
 public class StudentListAction extends Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		//TODO 自動生成されたメソッド・スタブ
+		HttpSession session = request.getSession();//セッション
+		Teacher teacher = (Teacher)session.getAttribute("user");
+
+		String entYearStr="";//入力された入学年度
+		String classNum="";//入力されたクラス番号
+		String isAttendStr="";//入力された在学フラグ
+		int entYear=0;//入学年度
+		boolean isAttend=false;//在学フラグ
+		List<Student> students=null;//学生リスト
+		LocalDate todayDate=LocalDate.now();//LocalDateインスタンスを取得
+		int year = todayDate.getYear();//現在の年を取得
+		StudentDao sDao=new StudentDao();//学生Dao
+		ClassNumDao cNumDao=newClassDao();//クラス番号Daoを初期化
+		Map<String, String>errors=new HashMap<>();//エラーメッセージ
 	}
 }
